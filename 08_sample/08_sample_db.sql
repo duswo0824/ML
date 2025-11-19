d3d6c65a65bab9c7aacf9a14938268813497dd8b
yeon <yeonjae@email.com>
2025-11-19 16:23:40 +0900

    08_sample

--- 08_sample/08_sample_db.sql
+++ 08_sample/08_sample_db.sql
@@ -3,10 +3,10 @@ desc news_score;
 
 select * from news_score ns;
 
-drop table news_score;
+-- drop table news_score;
 
 SELECT COUNT(score)AS cnt FROM news_score;
 
 select * from news_score ns where score >= 50;
 
-select subject, score from news_score ns;
+select subject, score from news_score;
